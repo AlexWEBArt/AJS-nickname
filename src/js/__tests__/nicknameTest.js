d952d7782558b379aecb78895be2d5009df666f8
Артемчук Алексей <elusiv@mail.ru>
2022-06-16 22:57:19 +0500

    esLint fix 16.06.22

--- src/js/__tests__/nicknameTest.js
+++ src/js/__tests__/nicknameTest.js
@@ -1,8 +1,8 @@
 import Validator from '../nickname';
 
 test('nickname validation', () => {
-    const result1 = new Validator('Elu332siv').validateUsername()
-    const result2 = new Validator('Elusiv').validateUsername()
-    expect(result1).toBe(false);
-    expect(result2).toBe(true);
+  const result1 = new Validator('Elu332siv').validateUsername();
+  const result2 = new Validator('Elusiv').validateUsername();
+  expect(result1).toBe(false);
+  expect(result2).toBe(true);
 });
--- src/js/nickname.js
+++ src/js/nickname.js
@@ -1,13 +1,12 @@
 export default class Validator {
-    constructor(name) {
-        this.name = name;
-    };
+  constructor(name) {
+    this.name = name;
+  }
 
-    validateUsername() {
-        if (/\d{3,}/.test(this.name)) {
-            return false
-        }
-        return /^[a-z][a-z0-9_-]*[a-z]$/i.test(this.name)
-    };
-  };
-  
+  validateUsername() {
+    if (/\d{3,}/.test(this.name)) {
+      return false;
+    }
+    return /^[a-z][a-z0-9_-]*[a-z]$/i.test(this.name);
+  }
+}
